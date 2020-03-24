c1f35a1cde1daf6c6290251b59cbe6cf451cb24f
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 21:38:55 -0400

    adding Nothing and Nothing+

---
+++ presences/Nothing+.js
@@ -0,0 +1,43 @@
+const info = module.exports = {
+    name: 'Nothing',
+    description: 'Play nothing for a few days',
+    author: 'MILLION#1321',
+    github: 'https://github.com/Million900o'
+};
+const rpc = require("discord-rpc");
+const logger = require('../logger.js');
+const client = new rpc.Client({ transport: 'ipc' });
+client.login({ clientId: '683017681376772116' });
+
+async function worker() {
+    setInterval(async () => {
+        let presence = {
+            startTimestamp: new Date(Date.now() - (69 * 24 * 60 * 60 * 1000)),
+            smallImageKey: 'untitled',
+            smallImageText: 'Yeet',
+            largeImageKey: 'untitled',
+            largeImageText: 'Wow, you found this!',
+            state: 'Playing Solo',
+            details: 'Not doing anything',
+            partySize: 420,
+            partyMax: 69,
+            instance: true
+        };
+        try {
+            await client.setActivity(presence);
+        } catch (e) {
+            logger.error(`Error ocurred while trying to update presence. Error: ${e}`);
+        }
+    }, 5000);
+}
+
+client.once('ready', () => {
+    logger.log(`Started up ${info.name} presence!`);
+    let presence = { state: 'Loading...' };
+    client.setActivity(presence);
+    worker();
+});
+client.on('error', (e) => {
+    logger.error(`Something went wrong while connecting to discord. Please try again.`);
+    logger.debug(`ERROR Event: ${e}`);
+});
--- presences/Nothing.js
+++ presences/Nothing.js
@@ -1,35 +1,37 @@
 const info = module.exports = {
-    name: 'Example presence',
-    description: 'This is a example presence implementation',
-    author: 'Roki_100#0230',
-    github: 'https://github.com/Roki100'
+    name: 'Nothing',
+    description: 'Play nothing for a few days',
+    author: 'MILLION#1321',
+    github: 'https://github.com/Million900o'
 };
+
 const rpc = require("discord-rpc");
 const logger = require('../logger.js');
+
 const client = new rpc.Client({ transport: 'ipc' });
-client.login({ clientId: '625733912953487370' });
+client.login({ clientId: '683017681376772116' });
 
 async function worker() {
     setInterval(async () => {
         let presence = {
-            state: `PresenceCord`,
-            details: `Example presence`,
-            instance: true
+            startTimestamp: new Date(Date.now() - (69 * 24 * 60 * 60 * 1000)),
+            instance: true,
         };
         try {
             await client.setActivity(presence);
         } catch (e) {
-            logger.error(`Error occured while trying to update presence. Error: ${e}`);
+            logger.error(`Error ocurred while trying to update presence. Error: ${e}`);
         }
     }, 5000);
 }
 
-
 client.once('ready', () => {
     logger.log(`Started up ${info.name} presence!`);
-    let presence = { state: 'Loading...' }; client.setActivity(presence);
+    let presence = { state: 'Loading...' };
+    client.setActivity(presence);
     worker();
 });
+
 client.on('error', (e) => {
     logger.error(`Something went wrong while connecting to discord. Please try again.`);
     logger.debug(`ERROR Event: ${e}`);
