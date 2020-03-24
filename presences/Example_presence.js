c3001b693465223a182f4a8dd58ecb70dc849ca8
Roki <35699619+Roki100@users.noreply.github.com>
2020-03-24 01:59:58 +0100

    delete not working concept of presence and add example one

--- presences/Example_presence.js
+++ presences/Example_presence.js
@@ -1,23 +1,19 @@
 const info = module.exports = {
-    name: 'System sensors',
-    description: 'Displays PC Temps/usage as presence (Customizable)',
+    name: 'Example presence',
+    description: 'This is a example presence implementation',
     author: 'Roki_100#0230',
     github: 'https://github.com/Roki100'
 }
 const rpc = require("discord-rpc");
 const logger = require('../logger.js');
-const sys = require('systeminformation');
 const client = new rpc.Client({ transport: 'ipc' });
 client.login({ clientId: '625733912953487370' });
 
 async function worker() {
 setInterval(async () => {
     let presence = {
-    state: `test`,
-    details: `test`,
-    //largeImageKey: `lol`,
-    //smallImageKey: `;p;`,
-    //smallImageText: `̶lol`,­
+    state: `PresenceCord`,
+    details: `Example presence`,
     instance: true
     };
     try {
